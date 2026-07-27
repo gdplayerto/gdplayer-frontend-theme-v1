41b6551ccd744e61de157d8a4a2bd12354e27638
GDPlayer <27673157+gdplayerto@users.noreply.github.com>
2026-07-27 15:47:32 +0800

    Perbarui changelog.twig dan sitemap.php

--- views/sitemap.php
+++ views/sitemap.php
@@ -6,7 +6,7 @@
  * File Created: Sunday, 6th August 2023 12:41:20 pm
  * Author: GDPlayer Developer (https://t.me/gdplayerto)
  * -----
- * Last Modified: Friday, 3rd July 2026 1:10:06 am
+ * Last Modified: Monday, 27th July 2026 3:37:19 pm
  * Modified By: GDPlayer Developer (https://t.me/gdplayerto>)
  * -----
  * Copyright 2020 - 2025 https://github.com/gdplayerto
@@ -17,50 +17,50 @@ declare(strict_types=1);
 session_write_close();
 
 header('Content-Type: application/xml; charset=UTF-8');
-createResponseHeaders(corsResponseHeaders(true));
+createResponseHeaders();
 
-$baseUrl = BASE_URL;
-
-echo <<<XML
+$content = <<<'XML'
 <?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
     xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
     http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     <url>
-        <loc>{$baseUrl}</loc>
+        <loc>%1$s/</loc>
         <lastmod>2021-11-19T07:47:44+00:00</lastmod>
         <priority>1.00</priority>
     </url>
     <url>
-        <loc>{$baseUrl}buy/</loc>
+        <loc>%1$s/buy/</loc>
         <lastmod>2026-07-02T17:00:00+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>{$baseUrl}buy-additional-features/</loc>
+        <loc>%1$s/buy-additional-features/</loc>
         <lastmod>2026-07-02T17:00:00+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>{$baseUrl}sharer/</loc>
+        <loc>%1$s/sharer/</loc>
         <lastmod>2021-11-19T07:47:44+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>{$baseUrl}changelog/</loc>
+        <loc>%1$s/changelog/</loc>
         <lastmod>2021-11-19T07:47:44+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>{$baseUrl}terms/</loc>
+        <loc>%1$s/terms/</loc>
         <lastmod>2021-11-19T07:47:44+00:00</lastmod>
         <priority>0.80</priority>
     </url>
     <url>
-        <loc>{$baseUrl}privacy/</loc>
+        <loc>%1$s/privacy/</loc>
         <lastmod>2021-11-19T07:47:44+00:00</lastmod>
         <priority>0.80</priority>
     </url>
 </urlset>
 XML;
+
+echo sprintf($content, rtrim(BASE_URL, '/'));
